d69596433a843fd82c2332f1d7d3baf768065140
rahid <hidayat.85@gmail.com>
2017-08-15 21:29:27 +0700

    tambah seed user

--- seeders/20170814041234-dummy-for-user.js
+++ seeders/20170814041234-dummy-for-user.js
@@ -13,9 +13,9 @@ module.exports = {
       }], {});
     */
     return queryInterface.bulkInsert('Users', [
-      {username:'johndoe', password:'foobar', role:'teacher', createdAt: new Date(), updatedAt: new Date()},
-      {username:'pakdengklek', password:'gogetgold', role:'academic', createdAt: new Date(), updatedAt: new Date()},
-      {username:'charlesxavier', password:'magnetowhy', role:'headmaster', createdAt: new Date(), updatedAt: new Date()}
+      {username:'guru', password:'0183518bfd1cb7b0d7d4cbd6224692de87a7c2b3fba99b7f39d9fdafacffa140', role:'teacher', salt:'1ciJTDN8', createdAt: new Date(), updatedAt: new Date()},
+      {username:'akademik', password:'ae100c861e89ebc8ce88312d64c6ee68d8c1ab07e7bc7cac7c2231ae1abf74ef', role:'academic', salt:'yXNL1Neh', createdAt: new Date(), updatedAt: new Date()},
+      {username:'kepsek', password:'a853a9904508270bc076d02a12737b401caf9aee766e23e05ff3c808e66510b7', role:'headmaster', salt:'7GJXHOOF', createdAt: new Date(), updatedAt: new Date()}
     ], {})
   },
 
